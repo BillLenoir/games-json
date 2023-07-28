b1efaf86dc32b70695865bef71bf0e5075d35a69
Bill Lenoir <bill@bill-lenoir.com>
2023-07-28 09:40:06 -0400

    Working Version using TYPESCRIPT, and some cleaning up

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-const express = require('express');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const fsApp = require("fs");
 const app = express();
-const fs = require('fs');
-const convert = require('xml-js');
-
 // Serves Express Yourself website
 app.use(express.static('public'));
-const PORT = process.env.PORT || 4001;
+let PORT;
+if (process.env.PORT != null) {
+    PORT = Number(process.env.PORT);
+}
+else {
+    PORT = 4001;
+}
 app.use(express.static('html'));
-
 // File names for the various data we store.
-const dataFiles = {
-  "have": './data/haveData.json',
-  "want": './data/wantData.json',
-  "gone": './data/hadData.json',
-  "selling": './data/sellingData.json'
-};
-
+// I've commented this out becuase I'm having difficulties
+// typing the sourceFile variable
+// const dataFiles: { have: string, want: string, gone: string, selling: string } = {
+//  have: './data/haveData.json',
+//  want: './data/wantData.json',
+//  gone: './data/hadData.json',
+//  selling: './data/sellingData.json'
+// }
 // Display the indicated page of the games from the indicated file
-app.get('/games/:file/:page', (req, res, next) => {
-
-  const sourceFile = dataFiles[req.params.file];
-
-  fs.readFile(sourceFile, (err, data) => {
-
-    if (err) {
-
-      console.log(err);
-    } else {
-
-      const readData = JSON.parse(data);
-      const currentPage = Number(req.params.page);
-      const from = (currentPage - 1) * 50;
-      const to = currentPage * 50;
-      const sendData = readData.slice(from,to);
-      sendData.push(Math.ceil(readData.length / 50));
-      res.send(sendData);
-
-    }
-  })
+// BILL ==> fix the ANY!!!
+app.get('/games/:file/:page', (req, res) => {
+    const url = req.url.split('/');
+    const sourceFile = `./data/${url[2]}Data.json`;
+    //    const sourceFile: string = dataFiles[req.params.file]
+    // BILL ==> fix the ANY!!!
+    fsApp.readFile(sourceFile, (err, data) => {
+        if (err != null) {
+            console.log(err);
+        }
+        else {
+            const readData = JSON.parse(data);
+            const currentPage = Number(url[3]);
+            const from = (currentPage - 1) * 50;
+            const to = currentPage * 50;
+            const sendData = readData.slice(from, to);
+            sendData.push(Math.ceil(readData.length / 50));
+            res.send(sendData);
+        }
+    });
 });
-
 // app.get('/expressions/:id', (req, res, next) => {
 //   const foundExpression = getElementById(req.params.id, expressions);
 //   if (foundExpression) {
@@ -48,18 +52,13 @@ app.get('/games/:file/:page', (req, res, next) => {
 //     res.status(404).send();
 //   }
 // });
-
 // app.put('/expressions/:id', (req, res, next) => {
 //   console.log(res);
 //   if (getElementById(req.params.id)) {
 //     updateElement(req.params.id, req.query, expressions);
 //   }
 // });
-
-
 // Start the server
 app.listen(PORT, () => {
-
-  console.log(`Listening on port ${PORT}`);
-
+    console.log(`Listening on port ${PORT}`);
 });
--- app.ts
+++ app.ts
@@ -1,41 +1,63 @@
-import express from "express"
+import express from 'express'
 import { readFile } from 'fs/promises'
-const fsApp = express()
+const app = express()
 
 // Serves Express Yourself website
-fsApp.use(express.static('public'))
+app.use(express.static('public'))
 
 let PORT: number
-if (process.env.PORT != null) {
-  PORT = Number(process.env.PORT)
+if (process.env.PORT != null && typeof process.env.PORT === 'number') {
+  PORT = process.env.PORT
 } else {
   PORT = 4001
 }
 
-fsApp.use(express.static('html'))
+app.use(express.static('html'))
 
-fsApp.get('/games/:file/:page', async (req: express.Request, res: express.Response) => {
+// Display the indicated page of the games from the indicated file
+app.get('/games/:file/:page', (req: express.Request, res: express.Response) => {
   const url: string[] = req.url.split('/')
   const sourceFile: string = `./data/${url[2]}Data.json`
+  const currentPage = Number(url[3])
 
-  fsApp.readFile(sourceFile)
-    .then((data: { toString: () => any }) => {
-      let readData = data.toString()
-      readData = JSON.parse(readData)
-      console.log(readData)
-      const currentPage = Number(url[3])
+  readSourceFile(sourceFile)
+    .then((data) => {
+      const readData = JSON.parse(data)
       const from = (currentPage - 1) * 50
       const to = currentPage * 50
       const sendData = readData.slice(from, to)
       sendData.push(Math.ceil(readData.length / 50))
       res.send(sendData)
     })
-    .catch((error: { message: any }) => {
+    .catch((error) => {
       console.error('Error reading file:', error.message)
       res.status(500).send('Error reading file')
     })
 })
 
-fsApp.listen(PORT, () => {
+async function readSourceFile (sourceFile: string): Promise<any> {
+  const fileData = await readFile(sourceFile)
+  const returnData = JSON.parse(fileData.toString())
+  return returnData
+}
+
+// app.get('/expressions/:id', (req, res, next) => {
+//   const foundExpression = getElementById(req.params.id, expressions);
+//   if (foundExpression) {
+//     res.send(foundExpression);
+//   } else {
+//     res.status(404).send();
+//   }
+// });
+
+// app.put('/expressions/:id', (req, res, next) => {
+//   console.log(res);
+//   if (getElementById(req.params.id)) {
+//     updateElement(req.params.id, req.query, expressions);
+//   }
+// });
+
+// Start the server
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
